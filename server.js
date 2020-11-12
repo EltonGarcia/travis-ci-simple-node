da00be6a5a96494fc04f75e6bb90bd1ab525dcdc
Elton Santana <elton.garcia.s@gmail.com>
2020-11-12 10:57:01 -0300

    Print env variable from Travis CI

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ function sleep(ms) {
 }
 
 async function main() {
+  const envVar = process.env.TRAVIS_CI_ENV_VAR;
   while(true) {
     console.log('Travis CI containers rule!');
+    if (envVar) {
+      console.log('Env. variable value is: ' + envVar);
+    } else {
+      console.log('no env var set');
+    }
     await sleep(5000);
   }
 }
